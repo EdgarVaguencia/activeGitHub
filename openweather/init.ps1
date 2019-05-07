a2e91c24935621671149e2044c2f5700d99d4bf9
Edgar Valencia <edgar@maices.com>
2019-05-07 16:49:29 -0500

    Suporte para Windows Task

---
+++ openweather/init.ps1
@@ -0,0 +1,16 @@
+$DocDir = [Environment]::GetFolderPath("MyDocuments")
+$CurrentFolder = $DocDir + '\activeGitHub\openweather'
+$Year = Get-Date -UFormat %Y
+$Month = Get-Date -UFormat %m
+
+Set-Location -Path $CurrentFolder
+
+if (!(Test-Path -Path $Year)) {
+  New-Item -Path $Year -ItemType Directory
+}
+
+if (!(Test-Path -Path "${Year}\\${Month}")) {
+  New-Item -Path "$Year\$Month" -ItemType Directory
+}
+
+python script.py --path "$Year/$Month"
